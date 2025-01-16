bef904487f44f168d93f6d9774a98f5a60e2d290
flammabl3 <harryjung135@gmail.com>
2025-01-16 09:28:16 -0700

    First commit (NON WORKING)

---
+++ app/vacation.ts
@@ -0,0 +1,9 @@
+export default function VacationDestination(place: string) {
+    return "Let's go to ${place}";
+}
+
+export const VacationPricing = {
+    getPrice: () => {
+        return 100;
+    }
+};
